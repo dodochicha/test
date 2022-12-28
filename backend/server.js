25657445e4d2fb6f58640f6c6158474264bbd9ae
dodochicha <kevincheng60303@gmail.com>
2022-12-29 02:16:41 +0800

    a

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "../frontend", "build")));
   app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "build", "logo192.png"));
   });
 }
 
