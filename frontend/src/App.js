6f85c08c3707173e152910d3bcafdb9a51399fc2
dodochicha <kevincheng60303@gmail.com>
2023-01-05 16:02:53 +0800

    a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,135 +2,112 @@ import {
   Routes,
   Route,
   Link,
-  NavLink,
-  BrowserRouter,
-  useNavigate,
   Navigate,
-  useLocation,
+  BrowserRouter,
+  Outlet,
 } from "react-router-dom";
 import React from "react";
-const fakeAuth = () =>
-  new Promise((resolve) => {
-    setTimeout(() => resolve("2342f2f1d131rf12"), 250);
-  });
-const Home = () => {
-  const { onLogin } = useAuth();
-  return (
-    <>
-      <h2>Home (Public)</h2>
-
-      <button type="button" onClick={onLogin}>
-        Sign In
-      </button>
-    </>
-  );
-};
-const useAuth = () => {
-  return React.useContext(AuthContext);
-};
-const Dashboard = () => {
-  const { token } = useAuth();
-  return (
-    <>
-      <h2>Dashboard (Protected)</h2>
-      <div>Authenticated as {token}</div>
-    </>
-  );
-};
-const AuthContext = React.createContext(null);
-const AuthProvider = ({ children }) => {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const [token, setToken] = React.useState(null);
-
-  const handleLogin = async () => {
-    const token = await fakeAuth();
-
-    setToken(token);
-    const origin = location.state?.from?.pathname || "/dashboard";
-    // console.log(
-    //   "location.state?.from?.pathname:",
-    //   location.state?.from?.pathname
-    // );
-    navigate(origin);
-  };
-
-  const handleLogout = () => {
-    setToken(null);
-  };
-
-  const value = {
-    token,
-    onLogin: handleLogin,
-    onLogout: handleLogout,
-  };
-
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-};
-const ProtectedRoute = ({ children }) => {
-  const { token } = useAuth();
-  const location = useLocation();
-  if (!token) {
-    return <Navigate to="/home" replace state={{ from: location }} />;
-  }
-
-  return children;
-};
 const App = () => {
+  const [user, setUser] = React.useState(null);
+
+  const handleLogin = () =>
+    setUser({
+      id: "1",
+      name: "robin",
+      permissions: ["analyze"],
+      roles: ["admin"],
+    });
+  const handleLogout = () => setUser(null);
+
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <h1>React Router</h1>
+      <h1>React Router</h1>
 
-        <Navigation />
-        <Routes>
-          <Route index element={<Home />} />
+      <Navigation />
+
+      {user ? (
+        <button onClick={handleLogout}>Sign Out</button>
+      ) : (
+        <button onClick={handleLogin}>Sign In</button>
+      )}
+
+      <Routes>
+        <Route index element={<Landing />} />
+        <Route path="landing" element={<Landing />} />
+        <Route element={<ProtectedRoute isAllowed={!!user} />}>
           <Route path="home" element={<Home />} />
-          <Route
-            path="dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="admin"
-            element={
-              <ProtectedRoute>
-                <Admin />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="*" element={<div>hi</div>} />
-        </Routes>
-      </AuthProvider>
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+        <Route
+          path="analytics"
+          element={
+            <ProtectedRoute
+              redirectPath="/home"
+              isAllowed={!!user && user.permissions.includes("analyze")}
+            >
+              <Analytics />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="admin"
+          element={
+            <ProtectedRoute
+              redirectPath="/home"
+              isAllowed={!!user && user.roles.includes("admin")}
+            >
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<p>There's nothing here: 404!</p>} />
+      </Routes>
     </BrowserRouter>
   );
 };
-const Navigation = () => {
-  const { onLogout, token } = useAuth();
-  // console.log(token);
+
+const Navigation = () => (
+  <nav>
+    <Link to="/landing">Landing</Link>
+    <Link to="/home">Home</Link>
+    <Link to="/dashboard">Dashboard</Link>
+    <Link to="/analytics">Analytics</Link>
+    <Link to="/admin">Admin</Link>
+  </nav>
+);
+
+const Landing = () => {
+  return <h2>Landing (Public: anyone can access this page)</h2>;
+};
+const Home = () => {
+  return <h2>Home (Protected: authenticated user required)</h2>;
+};
+
+const Dashboard = () => {
+  return <h2>Dashboard (Protected: authenticated user required)</h2>;
+};
+
+const Analytics = () => {
   return (
-    <nav>
-      <NavLink to="/home">Home</NavLink>
-      <NavLink to="/dashboard">Dashboard</NavLink>
-      <NavLink to="/admin">Admin</NavLink>
-      {token && (
-        <button type="button" onClick={onLogout}>
-          Sign Out
-        </button>
-      )}
-    </nav>
+    <h2>
+      Analytics (Protected: authenticated user with permission 'analyze'
+      required)
+    </h2>
   );
 };
+
 const Admin = () => {
   return (
-    <>
-      <h2>Admin (Protected)</h2>
-    </>
+    <h2>Admin (Protected: authenticated user with role 'admin' required)</h2>
   );
+};
+
+const ProtectedRoute = ({ isAllowed, redirectPath = "/landing", children }) => {
+  if (!isAllowed) {
+    return <Navigate to={redirectPath} replace />;
+  }
+
+  return children ? children : <Outlet />;
 };
 
 export default App;
